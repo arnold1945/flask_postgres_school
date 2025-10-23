01b2ee017d3c3e052af0b4933a529f81904a344a
arnold1945 <vscj58@gmail.com>
2025-10-23 00:06:07 -0500

    Im going to sleep

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = ('postgresql+psycopg:///students')
+
+db = SQLAlchemy(app)
+
+
+class Students(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    age = db.Column(db.Integer)
+    subject = db.Column(db.Integer)
+    
+    def __repr__(self):
+        return f'{self.id} {self.first_name}'
+
+def student_serializer(stud: Students) ->dict: #these are hints
+    return {
+        'id' : stud.id,
+        'first_name' : stud.first_name,
+        'last_name' : stud.last_name,
+        'age' : stud.age,
+        'subject' : stud.subject
+    }
+    
+#below will be the start of my app.routes. will need to do for subject and teachers as well
+
+@app.route('/') #don't forget the @ symbol
+def home():
+    return '<h1> WELCOME TO MY HOMEPAGE</h1>'
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port= 8000)
---
+++ school.sql
@@ -0,0 +1,36 @@
+
+
+--this is my schema
+
+DROP TABLE IF EXISTS student;
+
+CREATE TABLE student (
+    id serial PRIMARY KEY,
+    first_name varchar(255) NOT NULL,
+    last_name varchar(255) NOT NULL,
+    age INTEGER,
+    subject INTEGER
+);
+
+
+DROP TABLE IF EXISTS subjects;
+
+CREATE TABLE subjects (
+    id serial PRIMARY   KEY,
+    subject varchar(255) NOT NULL
+);
+
+DROP TABLE IF EXISTS teachers;
+CREATE TABLE teachers (
+    id serial PRIMARY KEY,
+    first_name varchar(255) NOT NULL,
+    last_name varchar(255) NOT NULL,
+    age INTEGER,
+    subject varchar(255)
+);
+--below connects sql file to the csv files
+\COPY student FROM 'data/student.csv' DELIMITER ',' CSV HEADER;
+\COPY subjects FROM 'data/subjects.csv' DELIMITER ',' CSV HEADER;
+\COPY teachers FROM 'data/teachers.csv' DELIMITER ',' CSV HEADER;
+
+SELECT * FROM student, subjects, teachers;
